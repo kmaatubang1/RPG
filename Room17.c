41426072e74c40ebd26585979b4ab5dc1eb69b92
Kevin Maatubang <kmaatubang1@nsmb208-xx.csudh.edu>
2018-03-19 17:17:04 -0700

    Version 3

--- Room17.c
+++ Room17.c
@@ -12,8 +12,30 @@ int main(void)
 	printf("You have a few choices to continue progressing\n");
 	printf("1 - Navigate to a specific room\n");
 	printf("2 - Wait until help arrives\n");
-	printf("3 - End it yourself by self immolation\n");
-	printf("-999 - Exit the program");
+	printf("3 - End the suffering yourself by self immolation\n");
+	printf("-999 - Exit the program\n");
+
+	scanf("%d", &choice);
+	switch(choice)
+	{	
+		case 1:
+			//Enter code to select a room number
+			break;
 	
+		case 2:
+			//Enter code that shows a random number of time passing	
+			//If too much time passes, enter a message that says you did nothing to progress, you died
+			break;
+		case 3:
+			printf("You are litterally on fire, you died a painful death");
+			break;
+		case 4:
+			printf("Quitter");
+			break;	
+	
+
+
+	switch(choice)
+	}
 	return 0;
 }	
