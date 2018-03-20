7e51840b8bd1c350cf3e8709d0b7a56497fe8967
Kevin Maatubang <kmaatubang1@nsmb208-xx.csudh.edu>
2018-03-19 17:07:11 -0700

    Version 3

--- Room17.c
+++ Room17.c
@@ -6,5 +6,14 @@
 
 int main(void)
 {
-	printf("This is room 17");
+	int choice;
+
+	printf("You are currently in room 17\n");
+	printf("You have a few choices to continue progressing\n");
+	printf("1 - Navigate to a specific room\n");
+	printf("2 - Wait until help arrives\n");
+	printf("3 - End it yourself by self immolation\n");
+	printf("-999 - Exit the program");
+	
+	return 0;
 }	
